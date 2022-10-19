6949d04cb2170f502a6b4c30f959d0fe168411b2
JJIYOM <57945918+JJIYOM@users.noreply.github.com>
2022-10-19 22:27:49 +0900

    수정

--- KJY/프린터(스택,큐).java
+++ KJY/프린터(스택,큐).java
@@ -1,13 +1,13 @@
-import java.util.;
+import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
         
-        QueueInteger docs = new LinkedList();
-        QueueInteger prints = new LinkedList();
+        Queue<Integer> docs = new LinkedList<>();
+        Queue<Integer> prints = new LinkedList<>();
         
-        for(int i=0; ipriorities.length; i++){
+        for(int i=0; i<priorities.length; i++){
             docs.offer(i);
             prints.offer(priorities[i]);
         }
@@ -19,12 +19,12 @@ class Solution {
             head = prints.poll();
             index = docs.poll();
             
-            for(int value  prints){
-                if(head  value){  나머지 중 존재
+            for(int value : prints){
+                if(head < value){ // 나머지 중 존재
                     prints.offer(head);
                     docs.offer(index);
                     answer--;
-                    index = -1;  뽑지 않으므로
+                    index = -1; // 뽑지 않으므로
                     break;
                 }
             }
